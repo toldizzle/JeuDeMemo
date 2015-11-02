d8b1861c9f72f5e1439633cea1537835ab0fce8f
Francis <toldizzle@hotmail.com>
2015-11-02 14:45:58 -0500

    CLasses

---
+++ JeuDeMemo/Utilisateur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JeuDeMemo
+{
+    public class Utilisateur
+    {
+        public int ID { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+
+    }
+}
