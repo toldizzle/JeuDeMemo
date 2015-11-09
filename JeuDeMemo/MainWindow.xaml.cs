f4031913af4172d00c0d4d6755ce04da0c280d64
Francis <toldizzle@hotmail.com>
2015-11-09 14:41:44 -0500

    fonctionnement chkBoxes

--- JeuDeMemo/MainWindow.xaml.cs
+++ JeuDeMemo/MainWindow.xaml.cs
@@ -24,5 +24,45 @@ namespace JeuDeMemo
         {
             InitializeComponent();
         }
+
+        private void chk8x8_Checked(object sender, RoutedEventArgs e)
+        {
+            chk9x9.IsChecked = false;
+        }
+
+        private void chk9x9_Checked(object sender, RoutedEventArgs e)
+        {
+            chk8x8.IsChecked = false;
+        }
+
+        private void chkJoueur1_Checked(object sender, RoutedEventArgs e)
+        {
+            chkJoueur2.IsChecked = false;
+        }
+
+        private void chkJoueur2_Checked(object sender, RoutedEventArgs e)
+        {
+            chkJoueur1.IsChecked = false;
+        }
+
+        private void chkFruits_Checked(object sender, RoutedEventArgs e)
+        {
+            chkVoitures.IsChecked = false;
+        }
+
+        private void chkVoitures_Checked(object sender, RoutedEventArgs e)
+        {
+            chkFruits.IsChecked = false;
+        }
+
+        private void chkDebut2_Checked(object sender, RoutedEventArgs e)
+        {
+            chkDebut1.IsChecked = false;
+        }
+
+        private void chkDebut1_Checked(object sender, RoutedEventArgs e)
+        {
+            chkDebut2.IsChecked = false;
+        }
     }
 }
