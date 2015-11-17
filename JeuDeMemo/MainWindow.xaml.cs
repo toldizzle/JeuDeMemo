633391f52667d6151ca5ac391820c4cdc55be089
toldizzle <toldizzle@hotmail.com>
2015-11-17 09:59:53 -0500

    début de tour et choix ordinateur 50%

--- JeuDeMemo/MainWindow.xaml.cs
+++ JeuDeMemo/MainWindow.xaml.cs
@@ -25,34 +25,36 @@ namespace JeuDeMemo
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Tours
         bool _bTourSysteme = false;
         bool _bContreSysteme = false;
+        bool _bTourJ1 = false;
+        bool _bTourJ2 = false;
+        int _pointageJ1 = 0;
+        int _pointageJ2 = 0;
         byte _bGrandeur = 0;
         int _premierChoix = 0;
         int _deuxiemeChoix = 0;
+        //Noms des boutons
         string _btn1;
         string _btn2;
         bool _bTheme = false; //false = fruit      true = voiture
-        bool _bTourJ1 = false;
-        bool _bTourJ2 = false;
+
         List<int> _lstImages = new List<int>();
         List<int> _lstRandom = new List<int>();
         Random rand = new Random();
         int _iChoixSysteme;
+        bool _bFinDePartie = false;
         public MainWindow()
         {
             InitializeComponent();
-            
+
         }
         public async void JeuMemoire()
         {
-            bool bFin = false;
-
-
             //Match des cartes
             if (_premierChoix == _deuxiemeChoix)
             {
-                
                 //Retourne les deux cartes
                 foreach (var item in _lstRandom)
                 {
@@ -71,8 +73,20 @@ namespace JeuDeMemo
                         btn.IsEnabled = false;
                     }
                 }
+                //Gain de point
+                if (_bTourJ1)
+                {
+                    _pointageJ1++;
+                    lblJ1Point.Content = _pointageJ1;
+                }
+                else
+                {
+                    _pointageJ2++;
+                    lblJ2Point.Content = _pointageJ2;
+                }
                 _premierChoix = 0;
                 _deuxiemeChoix = 0;
+                ChangementTour();
             }
             //Différentes cartes
             else if (_premierChoix != 0 && _deuxiemeChoix != 0)
@@ -86,7 +100,7 @@ namespace JeuDeMemo
                     {
                         //Retourne les cartes
                         Button btn = (Button)item;
-                       
+
                         if (btn.Name == _btn1)
                             btn.Background = RecevoirCarteDefaut();
 
@@ -98,12 +112,10 @@ namespace JeuDeMemo
                 };
                 _premierChoix = 0;
                 _deuxiemeChoix = 0;
+                ChangementTour();
             }
-           
-            //while (!bFin)
-            //{
-
-            //}
+            if (_bTourSysteme)
+                ChoixOrdinateur();
         }
         #region Boutons
         private void chk8x8_Checked(object sender, RoutedEventArgs e)
@@ -120,7 +132,7 @@ namespace JeuDeMemo
         {
             chkJoueur2.IsChecked = false;
             lblJ2.Content = "Système";
-            
+
         }
 
         private void chkJoueur2_Checked(object sender, RoutedEventArgs e)
@@ -159,15 +171,6 @@ namespace JeuDeMemo
                 int iTag = 0;
                 Options.IsEnabled = false;
                 btnDemarrer.IsEnabled = false;
-                //Tour de jeu
-                if (chkJoueur1.IsChecked == true)
-                    _bContreSysteme = true;
-                if (chkDebut1.IsChecked == true)
-                    _bTourJ1 = true;
-                else if (chkJoueur2.IsChecked == true && _bContreSysteme == false)
-                    _bTourJ2 = true;
-                else
-                    _bTourSysteme = true;
 
                 if (chk8x8.IsChecked == true)
                 {
@@ -207,29 +210,40 @@ namespace JeuDeMemo
                     _lstImages.Add(i);
                 }
                 _lstRandom = _lstImages.OrderBy(c => rand.Next()).Select(c => c).ToList();
+                //Tour de jeu
+                if (chkJoueur1.IsChecked == true)
+                    _bContreSysteme = true;
+                if (chkDebut1.IsChecked == true)
+                    _bTourJ1 = true;
+                else if (chkJoueur2.IsChecked == true && _bContreSysteme == false)
+                    _bTourJ2 = true;
+                else
+                    _bTourSysteme = true;
             }
             else
                 MessageBox.Show("Vous n'avez pas rempli la grille d'option.");
+            
 
         }
         void button_Click(object sender, RoutedEventArgs e)
         {
-
-            int iTag = int.Parse(string.Format("{0}", (sender as Button).Tag));
-            if (_premierChoix == 0)
+            if (!_bTourSysteme)
             {
-                _premierChoix = _lstRandom[int.Parse(string.Format("{0}", (sender as Button).Tag))];
-                _btn1 = (sender as Button).Name;
+                int iTag = int.Parse(string.Format("{0}", (sender as Button).Tag));
+                if (_premierChoix == 0)
+                {
+                    _premierChoix = _lstRandom[int.Parse(string.Format("{0}", (sender as Button).Tag))];
+                    _btn1 = (sender as Button).Name;
+                }
+                else if (_deuxiemeChoix == 0)
+                {
+                    _deuxiemeChoix = _lstRandom[int.Parse(string.Format("{0}", (sender as Button).Tag))];
+                    _btn2 = (sender as Button).Name;
+                }
+                (sender as Button).Background = RecevoirInfoBouton(iTag);
+                (sender as Button).IsHitTestVisible = false;
+                JeuMemoire();
             }
-            else if (_deuxiemeChoix == 0)
-            {
-                _deuxiemeChoix = _lstRandom[int.Parse(string.Format("{0}", (sender as Button).Tag))];
-                _btn2 = (sender as Button).Name;
-            }
-            (sender as Button).Background = RecevoirInfoBouton(iTag);
-            (sender as Button).IsHitTestVisible = false;
-            JeuMemoire();
-
         }
 
         private void btnRecommencer_Click(object sender, RoutedEventArgs e)
@@ -237,7 +251,15 @@ namespace JeuDeMemo
             this.Jeu.Children.Clear();
             btnDemarrer.IsEnabled = true;
             Options.IsEnabled = true;
-
+            _bTourSysteme = false;
+            _bTourJ1 = false;
+            _bTourJ2 = false;
+            _deuxiemeChoix = 0;
+            _premierChoix = 0;
+            _pointageJ1 = 0;
+            _pointageJ2 = 0;
+            List<int> _lstImages = new List<int>();
+            List<int> _lstRandom = new List<int>();
         }
         #endregion
         private ImageBrush RecevoirInfoBouton(int tagBouton)
@@ -289,6 +311,47 @@ namespace JeuDeMemo
                 Button btn = (Button)item;
                 btn.IsHitTestVisible = true;
             }
+        }
+        private void ChangementTour()
+        {
+            if(_bContreSysteme)
+            {
+                _bTourSysteme = !_bTourSysteme;
+                _bTourJ1 = !_bTourJ1;
+            }
+            else
+            {
+                _bTourJ1 = !_bTourJ1;
+                _bTourJ2 = !_bTourJ2;
+            }
+        }
+        private void ChoixOrdinateur()
+        {
+            _btn1 = "btn" + rand.Next(1,_lstImages.Count);
+            foreach (var item in Jeu.Children)
+            {
+                if ((item as Button).Name == _btn1)
+                {
+                    int iTag = int.Parse(string.Format("{0}", (item as Button).Tag));
+                    (item as Button).Background = RecevoirInfoBouton(iTag);
+                    _premierChoix = iTag;
+                }
+                    
+            }
+
+            _btn2 = "btn" + rand.Next(1,_lstImages.Count);
+            foreach(var item in Jeu.Children)
+            {
+                if ((item as Button).Name == _btn2)
+                {
+                    int iTag = int.Parse(string.Format("{0}", (item as Button).Tag));
+                    (item as Button).Background = RecevoirInfoBouton(iTag);
+                    _deuxiemeChoix = iTag;
+                }
+
+            }
+            JeuMemoire();
+
         }
     }
 }
